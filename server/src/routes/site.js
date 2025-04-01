0f67166ada676885cd4465b27f8b822a2e1df99e
KhoaDang113 <143401353+KhoaDang113@users.noreply.github.com>
2025-04-01 16:02:09 +0700

    Remove backend, keep only frontend

--- server/src/routes/site.js
+++
@@ -1,6 +0,0 @@
-const siteRouter = require("express").Router();
-const siteController = require("../controllers/SiteController");
-
-siteRouter.get("/api/category", siteController.getAllCategory);
-
-module.exports = siteRouter;
--- server/src/routes/webhook.js
+++
@@ -1,5 +0,0 @@
-const webhookRouter = require("express").Router();
-const { handleWebHook } = require("../controllers/WebhookController");
-
-webhookRouter.post("/", handleWebHook);
-module.exports = webhookRouter;
