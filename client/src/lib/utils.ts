4db7023bd5a554e20a0e3e66f71aeb57f8edc479
HuyUDB24CC149 <HuyUD.B24CC149@hotmail.com>
2025-04-19 15:08:14 +0700

    Fix order management UI and bugs, map API for search.

--- client/src/lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import { type ClassValue, clsx } from "clsx"; // cho sai cac prop nhu: chuỗi, mảng, đối tượng, điều kiện logic
-import { twMerge } from "tailwind-merge"; // code toi uu hon (loai bo cac thang trung nhau trong tailwind)
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
